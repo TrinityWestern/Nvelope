328195ba0aedeb12d194c4ecd3213ce48403d0ea
Alan Trick <alan.trick@twu.ca>
2011-05-31 14:32:45 -0700

    Switched lambdas out for a predefined function so the code would be covered

--- test/IndexingExtensionTests.cs
+++ test/IndexingExtensionTests.cs
@@ -9,25 +9,29 @@ namespace Nvelope.Tests
     [TestFixture]
     public class IndexingExtensionTests
     {
+        Func<string, int> GetLength = new Func<string, int>(
+            (string s) => { return s.Length; });
+
+
         [Test]
         public void IndexFlat()
         {
-            Assert.AreEqual("([1,a],[2,bb])", new string[] { "a", "bb" }.IndexFlat(s => s.Length).Print());
-            Assert.AreEqual("()", new string[] { }.IndexFlat(s => s).Print());
+            Assert.AreEqual("([1,a],[2,bb])", new string[] { "a", "bb" }.IndexFlat(GetLength).Print());
+            Assert.AreEqual("()", new string[] { }.IndexFlat(GetLength).Print());
         }
 
         [Test]
         public void Index()
         {
-            Assert.AreEqual("([1,(a,b)],[2,(aa)])", new string[] { "a", "aa", "b" }.Index(s => s.Length).Print());
-            Assert.AreEqual("()", new string[] { }.Index(s => s).Print());
+            Assert.AreEqual("([1,(a,b)],[2,(aa)])", new string[] { "a", "aa", "b" }.Index(GetLength).Print());
+            Assert.AreEqual("()", new string[] { }.Index(GetLength).Print());
         }
 
         [Test]
         public void Map()
         {
-            Assert.AreEqual("([a,1],[b,1],[bb,2])", new string[] { "a", "b", "bb" }.MapIndex(s => s.Length).Print());
-            Assert.AreEqual("()", new string[] { }.MapIndex(s => s.Length).Print());
+            Assert.AreEqual("([a,1],[b,1],[bb,2])", new string[] { "a", "b", "bb" }.MapIndex(GetLength).Print());
+            Assert.AreEqual("()", new string[] { }.MapIndex(GetLength).Print());
         }
     }
 }
