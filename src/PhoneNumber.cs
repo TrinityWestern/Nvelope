10b87daa6dae64f43320de35ccce0be80c71af27
Alan Trick <alan.trick@twu.ca>
2011-06-01 17:56:56 -0700

    New phone number code from aqueduct

---
+++ src/PhoneNumber.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace Nvelope
+{
+    /// <remark>
+    /// Some of the stuff in here might be locale specific
+    /// </remark>
+    public class PhoneNumber
+    {
+        public static Regex StringFormat = new Regex("^(?<countryBlock> *(?<country>[\\d]{1,4})([ \\.\\-]+))?"
+                + "(?<areaBlock>(?<area>[\\d]{1,4})([ \\.\\-]+))?"
+                + "(?<localBlock>(?<local>[\\d\\-\\.]{6,16}))"
+                + "(?<extensionBlock>\\s*(x|ext([\\.:]))? *(?<extension>[\\d]{1,10})? *)?$");
+
+        public string Country = "";
+        public string Area = "";
+        public string Local = "";
+        public string Extension = "";
+
+        public PhoneNumber(string str)
+        {
+            var match = PhoneNumber.StringFormat.Match(str);
+            if (!match.Success)
+                throw new ArgumentOutOfRangeException(
+                    "'" + str + "' is not a parseable phone number.");
+            this.Country = match.Groups["country"].Value;
+            this.Area = match.Groups["area"].Value;
+            this.Local = match.Groups["local"].Value;
+            this.Extension = match.Groups["extension"].Value;
+        }
+
+        /// <summary>
+        /// A phone constructor where you set you own parts
+        /// </summary>
+        public PhoneNumber() {}
+
+        public override string ToString() {
+            string result = this.Local;
+            if (this.Area != "")      result = this.Area + "-" + result;
+            if (this.Country != "")   result = this.Country + "-" + result;
+            if (this.Extension != "") result += "x" + this.Extension;
+            return result;
+        }
+    }
+}
---
+++ test/PhoneNumberTests.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using NUnit.Framework;
+
+namespace Nvelope.Tests
+{
+    [TestFixture]
+    class PhoneNumberTests
+    {
+        [Test]
+        public void ValidNumbers()
+        {
+            var number = new PhoneNumber("7788782908");
+            Assert.AreEqual("7788782908", number.Local);
+
+            number = new PhoneNumber("778-878-2908");
+            Assert.AreEqual("878-2908", number.Local);
+
+            // This seems kinda wierd :-/
+            number = new PhoneNumber("878-2908");
+            Assert.AreEqual("878-2908", number.Local);
+
+            number = new PhoneNumber("1-878-443-2908 ext. 23");
+            Assert.AreEqual("1", number.Country);
+            Assert.AreEqual("878", number.Area);
+            Assert.AreEqual("443-2908", number.Local);
+            Assert.AreEqual("23", number.Extension);
+
+            number = new PhoneNumber("1-878-443-2908x23");
+            Assert.AreEqual("1", number.Country);
+            Assert.AreEqual("878", number.Area);
+            Assert.AreEqual("443-2908", number.Local);
+            Assert.AreEqual("23", number.Extension);
+        }
+
+        [Test]
+        public void InValidNumbers()
+        {
+            Assert.Throws<ArgumentOutOfRangeException>(() => new PhoneNumber(""));
+        }
+
+        [Test]
+        public void Print()
+        {
+            var number = new PhoneNumber("1-878-443-2908 ext. 23");
+            Assert.AreEqual("1-878-443-2908x23", number.ToString());
+        }
+    }
+}
