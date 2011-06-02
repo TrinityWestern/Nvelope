71bc39d9663d2a1c515235e599751d177f34f9a7
Alan Trick <alan.trick@twu.ca>
2011-06-02 16:06:33 -0700

    This method is just going to stay this way, maybe a Readlines function can be added in the future

--- src/IO/TextFile.cs
+++ src/IO/TextFile.cs
@@ -27,17 +27,11 @@ namespace Nvelope.IO
         /// Reads in text from a file. Returns empty list if the file doesn't exist
         /// </summary>
         /// <remarks>Stolen from Clojure</remarks>
-        /// <param name="filename"></param>
-        /// <returns></returns>
         public static string Slurp(string filename)
         {
             if (!File.Exists(filename))
                 return "";
-
-            // TODO: Make this function chunky-lazy (read in like 1000 chars at a time, and use an iterator to return them)
-
-            using (var f = new StreamReader(filename))
-            {
+            using (var f = new StreamReader(filename)) {
                 var res = f.ReadToEnd();
                 f.Close();
                 return res;
