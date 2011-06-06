TrinityWestern/Nvelope

commit f4548c0c86eacaba6463f35836637003a76cce22

Split out Print(). Instead of checking types dynamically, it now does it statically.

src/ObjectExtensions.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-
-namespace Nvelope
+﻿namespace Nvelope
 {
     public static class ObjectExtensions
     {
@@ -21,6 +16,14 @@ namespace Nvelope
                 return false;
 
             return obj.Equals(other);
+        }
+
+        /// <summary>
+        /// Like ToString, but it handles nulls and gives nicer results for
+        /// some objects.
+        /// </summary>
+        public static string Print(this object o) {
+                return o.ToStringN();
         }
     }
 }

src/ConversionExtensions.cs
@@ -1,13 +1,10 @@
 ﻿using System;
-using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
-using System.Text;
-using System.Xml;
 using System.Reflection;
 using System.Text.RegularExpressions;
-using System.Globalization;
+using System.Xml;
 using Nvelope.Exceptions;
-using Nvelope.Reflection;
 
 namespace Nvelope
 {
@@ -16,37 +13,6 @@ namespace Nvelope
     /// </summary>
     public static class ConversionExtensions
     {
-        public static string Print(this object o)
-        {
-            // Decimals don't do ToString in a reasonable way
-            // It's really irritating
-            // So chop off the trailing .0000 if it has it
-            // TODO: Split out https://radar.twu.ca/issues/3245
-            if (o is decimal)
-            {
-                var parts = o.ToStringN().Split('.');
-                if (parts.Count() == 1)
-                    return parts.First();
-                else if (parts.Second().All(c => c == '0')) // if it's all 0s, drop the decimal part
-                    return parts.First();
-                else
-                    return parts.First() + "." + Regex.Replace(parts.Second(), "0*$", ""); // trim off trailing 0s
-
-            }
-            else
-                return o.ToStringN();
-        }
-
-        /// <remarks>This exists so that the compiler doesn't get confused and call the IEnumerable[char] version of Print
-        /// on a string. Then you would get "(a,b)" from "ab".Print(), which is silly</remarks>
-        /// <param name="s"></param>
-        /// <returns></returns>
-        
-        public static string Print(this string s)
-        {
-            return s.ToStringN();
-        }
-
         /// <summary>
         /// Is the string convertible to a bool?
         /// </summary>

src/NumberExtensions.cs
@@ -1,7 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
+using System.Text.RegularExpressions;
 
 namespace Nvelope
 {
@@ -124,6 +124,20 @@ namespace Nvelope
         public static bool IsAbout(this decimal number, decimal other, decimal precision)
         {
             return Math.Abs(number - other) < precision;
+        }
+
+        public static string Print(this decimal o)
+        {
+            // Decimals don't do ToString in a reasonable way
+            // It's really irritating
+            // So chop off the trailing .0000 if it has it
+            var parts = o.ToStringN().Split('.');
+            if (parts.Count() == 1)
+                return parts.First();
+            else if (parts.Second().All(c => c == '0')) // if it's all 0s, drop the decimal part
+                return parts.First();
+            else
+                return parts.First() + "." + Regex.Replace(parts.Second(), "0*$", ""); // trim off trailing 0s
         }
     }
 }

src/StringExtensions.cs
@@ -2,15 +2,19 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using System.Collections.Specialized;
-using System.Security;
 using System.Text.RegularExpressions;
-using System.Xml;
 
 namespace Nvelope
 {
     public static class StringExtensions
     {
+        /// <remarks>This exists so that the compiler doesn't get confused and call the IEnumerable[char] version of Print
+        /// on a string. Then you would get "(a,b)" from "ab".Print(), which is silly</remarks>
+        public static string Print(this string s)
+        {
+            return s.ToStringN();
+        }
+
         /// <summary>
         /// Returns a string array that contains the substrings in this string that are
         ///     delimited by elements of a specified string array. A parameter specifies

commit 6bf63766d0e1560789d433ca290a3e5c7ec3c122

Restored "polymorphic" behavior for object.Print()
Fixes a bug introduced in f4548c0c8

src/ObjectExtensions.cs
@@ -22,7 +22,19 @@
         /// Like ToString, but it handles nulls and gives nicer results for
         /// some objects.
         /// </summary>
-        public static string Print(this object o) {
+        public static string Print(this object o) 
+        {
+            // This function should also work polymorphically
+            // Sometimes, we've got variables of type object, but we want them to print 
+            // nicer than ToString() for their type (ie, for decimals, ToString() works stupidly)
+            // So we do shotgun polymorphism here to take care of that, since we can't 
+            // hack into the original types to override their ToString methods
+
+            // Decimals don't do ToString in a reasonable way
+            // It's really irritating
+            if (o is decimal)
+                return ((decimal)o).Print();
+            else
                 return o.ToStringN();
         }
     }

test/ObjectExtensionTests.cs
@@ -24,5 +24,22 @@ namespace Nvelope.Tests
             Assert.True(a.Eq(b));
             Assert.True(b.Eq(a));
         }
+
+        [Test]
+        public void PrintDecimalWorksPolymorphically()
+        {
+            var dec = 1.0000m;
+            object obj = dec;
+
+            Assert.AreEqual("1", dec.Print());
+            // We have a variable of type object, but it should
+            // still call the decimal version of print, not the
+            // generic object ToString version. If we call decimal.ToString(),
+            // we'll get "1.0000"
+            Assert.AreNotEqual("1.0000", obj.Print(), 
+                "Print was not called polymorphically. You probably changed the object.Print() extension " + 
+                " method in a way you shouldn't have");
+            Assert.AreEqual("1", obj.Print());
+        }
     }
 }