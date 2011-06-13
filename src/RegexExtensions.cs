dbe6bfc8acdf2a5a17e55acd6f3d6c028600ceb7
brian.dejong <brian.dejong@twu.ca>
2011-06-13 13:52:28 -0700

    Added RegexExtensions

---
+++ src/RegexExtensions.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace Nvelope
+{
+    public static class RegexExtensions
+    {
+        public static IEnumerable<Group> ToList(this GroupCollection coll)
+        {
+            foreach (var g in coll)
+                yield return g as Group;
+        }
+
+        public static string Print(this Group group)
+        {
+            return group.Value;
+        }
+
+        public static string Print(this Match match)
+        {
+            return match.Groups.ToList().Print();
+        }
+    }
+}
---
+++ test/RegexExtensionTests.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+using Nvelope;
+using System.Text.RegularExpressions;
+
+namespace Nvelope.Tests
+{
+    [TestFixture]
+    public class RegexExtensionTests
+    {
+        [Test]
+        public void ToList()
+        {
+            var match = Regex.Match("abcd", ".(b).(d)");
+            var groups = match.Groups.ToList();
+            Assert.AreEqual(3, groups.Count());
+            Assert.AreEqual("abcd", groups.First().Value);
+            Assert.AreEqual("b", groups.Second().Value);
+            Assert.AreEqual("d", groups.Third().Value);
+        }
+
+        [Test]
+        public void Print()
+        {
+            var match = Regex.Match("abcd", ".(b).(d)");
+            Assert.AreEqual("(abcd,b,d)", match.Print());
+        }
+    }
+}
