33db4464c1d2513d8be22864fc58d3b2b17ac548
Alan Trick <alan.trick@twu.ca>
2011-07-12 15:57:12 -0700

    forgot to manually add files

---
+++ src/Collections/DictionaryMirror.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Nvelope.Collections
+{
+    /// <summary>
+    /// This establishes a one to one mapping between a's and b's.
+    /// Say you had CheckBoxes and CalendarTags: given any checkbox
+    /// you could find the associated tag; given any tag, you could
+    /// find the assoicated checkbox.
+    /// 
+    /// Technically: y(f(x)) = x
+    /// </summary>
+    /// <typeparam name="TKey"></typeparam>
+    /// <typeparam name="TValue"></typeparam>
+    public class DictionaryMirror<TKey, TValue>
+    {
+        private Dictionary<TKey, TValue> mappingA = new Dictionary<TKey, TValue>();
+        private Dictionary<TValue, TKey> mappingB = new Dictionary<TValue, TKey>();
+        private List<TKey> aList = new List<TKey>();
+        private List<TValue> bList = new List<TValue>();
+
+        public void Add(TKey key, TValue value)
+        {
+            mappingA.Add(key, value);
+            mappingB.Add(value, key);
+            aList.Add(key);
+            bList.Add(value);
+        }
+
+        public TKey this[TValue value]
+        {
+            get { return mappingB[value]; }
+        }
+
+        public TValue this[TKey value]
+        {
+            get { return mappingA[value]; }
+        }
+
+        public IEnumerable<TKey> Keys() 
+        {
+            return aList;
+        }
+
+        public IEnumerable<TValue> Values() 
+        {
+            return bList;
+        }
+    }
+}
---
+++ test/Collections/DictionaryMirrorTests.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+using Nvelope.Collections;
+
+namespace Nvelope.Tests.Collections
+{
+    [TestFixture]
+    public class DictionaryMirrorTests
+    {
+        [Test]
+        public void AddEtc()
+        {
+            var m = new DictionaryMirror<int, string>();
+            m.Add(1, "one");
+            m.Add(2, "two");
+
+            Assert.AreEqual(2, m["two"]);
+            Assert.AreEqual("two", m[2]);
+            Assert.AreEqual("(1,2)", m.Keys().Print());
+            Assert.AreEqual("(one,two)", m.Values().Print());
+        }
+    }
+}
