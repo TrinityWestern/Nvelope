e0fefd4a1345a8b8dfbe7a31b2406bac2991f8e1
brian.dejong <brian.dejong@twu.ca>
2011-08-24 11:25:14 -0700

    Added DescribeLength() - human readable timespans

--- src/TimeSpanExtensions.cs
+++ src/TimeSpanExtensions.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace Nvelope
 {
@@ -10,6 +12,57 @@ namespace Nvelope
         public static int RoundToYears(this TimeSpan source)
         {
             return (source.TotalDays / 365).ConvertTo<decimal>().RoundTo().ConvertTo<int>();
+        }
+
+        /// <summary>
+        /// Get a human-friendly approximate description of the length of the timespan
+        /// </summary>
+        /// <param name="interval"></param>
+        /// <returns></returns>
+        public static string DescribeLength(this TimeSpan interval)
+        {
+            const double second = 1000.0;
+            const double minute = 60 * second;
+            const double hour = 60 * minute;
+            const double day = 24 * hour;
+
+            // A mapping of a lower limit (inclusive) of milliseconds to a function
+            // that prints out a description of the time.
+            // How this works:
+            // The algorithm finds the smallest key where the number is greater than
+            // or equal to the number of milliseconds in the timespan, then uses the
+            // corresponding function to print out a description of the time
+            var printers = new Dictionary<double, Func<TimeSpan, string>>()
+            {        
+                { 1.1, t => "1 millisecond"},
+                { second * .9, t => t.Milliseconds + " milliseconds"},
+                { second * 1.5, t => "1 second"},
+                { second * 50, t => t.Seconds + " seconds"},
+                { minute * 1.5, t => "1 minute"},
+                { minute * 50, t => t.Minutes + " minutes"},
+                { minute * 70, t => "1 hour"},
+                { minute * 90, t => t.Minutes + " minutes"},
+                { hour * 23, t => t.Hours + " hours"},
+                { hour * 47, t => "1 day"},
+                { day * 6, t => t.Days + " days"},
+                { day * 8, t => "1 week"},
+                { day * 24, t => t.Days / 7 + " weeks"},
+                { day * 34, t => "1 month"},
+                { day * 350, t => t.Days / 30 + " months"},
+                { day * 380, t => "1 year"},
+                { day * 715, t => t.Days / 30 + " months"},
+                { day * 740, t => "2 years"},
+                { day * 1080, t => t.Days / 30 + " months"},
+                { day * 1110, t => "3 years"},
+                { day * 1445, t => t.Days / 30 + " months"},
+                {Double.MaxValue, t => t.Days / 365 + " years"}
+            };
+
+            var applicableKV = printers.OrderBy(kv => kv.Key)
+                .First(kv => kv.Key >= interval.TotalMilliseconds);
+            var printer = applicableKV.Value;
+
+            return printer(interval);
         }
     }
 }
--- test/TimeSpanExtensionTests.cs
+++ test/TimeSpanExtensionTests.cs
@@ -18,5 +18,53 @@ namespace Nvelope.Tests
             TimeSpan oneMonth = new TimeSpan(31, 0, 0, 0);
             Assert.AreEqual(0, oneMonth.RoundToYears());
         }
+
+        [Test]
+        public void DescribeLength()
+        {
+            var now = new DateTime(2011, 1, 24);
+            var second = now.AddSeconds(1);
+            var fiveSecond = now.AddSeconds(5);
+            var minute = now.AddMinutes(1);
+            var fiveMinute = now.AddMinutes(5);
+            var hour = now.AddHours(1);
+            var fiveHour = now.AddHours(5);
+            var day = now.AddDays(1);
+            var fiveDay = now.AddDays(5);
+            var week = now.AddDays(7);
+            var twoWeek = now.AddDays(14);
+            var month = new DateTime(2011, 2, 24);
+            var sixMonth = new DateTime(2011, 7, 24);
+            var year = new DateTime(2012, 1, 24);
+            var yearAndDay = new DateTime(2012, 1, 25);
+            var eighteenMonth = new DateTime(2012, 7, 24);
+            var twoYear = new DateTime(2013, 1, 24);
+            var thirtyfiveMonth = new DateTime(2013, 12, 24);
+            var eightPfiveYear = new DateTime(2019, 7, 24);
+
+            var fn = new Func<DateTime, string>(d => d.Subtract(now).DescribeLength());
+
+            Assert.AreEqual("1 second", fn(second));
+            Assert.AreEqual("5 seconds", fn(fiveSecond));
+            Assert.AreEqual("1 minute", fn(minute));
+            Assert.AreEqual("5 minutes", fn(fiveMinute));
+            Assert.AreEqual("1 hour", fn(hour));
+            Assert.AreEqual("5 hours", fn(fiveHour));
+            Assert.AreEqual("1 day", fn(day));
+            Assert.AreEqual("5 days", fn(fiveDay));
+            Assert.AreEqual("1 week", fn(week));
+            Assert.AreEqual("2 weeks", fn(twoWeek));
+            Assert.AreEqual("1 month", fn(month));
+            Assert.AreEqual("6 months", fn(sixMonth));
+            Assert.AreEqual("1 year", fn(year));
+            Assert.AreEqual("1 year", fn(yearAndDay),
+                "If it's close to a year, just call it a year");
+            Assert.AreEqual("18 months", fn(eighteenMonth),
+                "Small periods of years should be measured in months, unless they're complete years");
+            Assert.AreEqual("2 years", fn(twoYear));
+            Assert.AreEqual("35 months", fn(thirtyfiveMonth));
+            Assert.AreEqual("8 years", fn(eightPfiveYear),
+                "If it's a big enough number of years, don't bother counting it in months");
+        }
     }
 }
