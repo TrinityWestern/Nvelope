ffa1e48fb351cb07cbbbdf798006ab1e3417db43
badjer <brian.dejong@gmail.com>
2012-01-27 10:08:55 -0800

    Added Fn for functional-programming structures that don't work as extension methods

---
+++ src/Nvelope/Fn.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Nvelope
+{
+    public static class Fn
+    {
+        /// <summary>
+        /// Apply all of the supplied functions, one after the other, to the supplied input.
+        /// Equivalent to fns[2](fns[1](fns[0](obj)))... but more readable
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="obj"></param>
+        /// <param name="fns"></param>
+        /// <returns></returns>
+        public static T Thread<T>(T obj, params Func<T, T>[] fns)
+        {
+            if (fns == null)
+                return obj;
+
+            T cur = obj;
+            foreach (var fn in fns)
+                cur = fn(cur);
+
+            return cur;
+        }
+    }
+}
---
+++ test/Nvelope.Tests/FnTests.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+using Nvelope;
+
+namespace Nvelope.Tests
+{
+    [TestFixture]
+    public class FnTests
+    {
+        [Test]
+        public void Thread()
+        {
+            var i = 2;
+            var res = Fn.Thread(i, j => j + 1, j => j * 2);
+            // Should be equal to (2 + 1) * 2
+            Assert.AreEqual(6, res);
+        }
+    }
+}
