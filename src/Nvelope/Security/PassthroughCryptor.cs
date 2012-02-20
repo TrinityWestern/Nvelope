5357634197aa6276b3030913b95dfdce276b3f6e
badjer <brian.dejong@gmail.com>
2012-02-20 11:44:46 -0800

    Added a cryptor that does nothing

---
+++ src/Nvelope/Security/PassthroughCryptor.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Nvelope.Security
+{
+    /// <summary>
+    /// Implements "encryption" by doing nothing.
+    /// This class is handy as a dummy implementation of the ISymmetricCryptor
+    /// </summary>
+    public class PassthroughCryptor : ISymmetricCryptor
+    {
+        public string Encrypt(string plaintext)
+        {
+            return plaintext;
+        }
+
+        public string Decrypt(string cyphertext)
+        {
+            return cyphertext;
+        }
+    }
+}
