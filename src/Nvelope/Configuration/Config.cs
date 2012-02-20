4a9fa96f09228fe4b426c7ce63b2f5ec6e269c7b
badjer <brian.dejong@gmail.com>
2012-02-20 11:44:23 -0800

    Added method to access all connection strings

--- src/Nvelope/Configuration/Config.cs
+++ src/Nvelope/Configuration/Config.cs
@@ -60,6 +60,21 @@ namespace Nvelope.Configuration
             return res;
         }
 
+        public static IEnumerable<string> ConnectionStrings()
+        {
+            var allConnStrs = new List<string>();
+            foreach (ConnectionStringSettings cs in ConfigurationManager.ConnectionStrings)
+                allConnStrs.Add(cs.Name);
+
+            // Filter out anything that is just another version of the same connection string
+            // ie, if we've got sync-live, sync-dev, sync-cassini, we only want to return sync
+            // These are all the versions we want to dump
+            var locationized = allConnStrs.SelectMany(cs => GetLocationizedNames(cs).Except(cs));
+
+            var res = allConnStrs.Except(locationized);
+            return res;
+        }
+
         public static string ConnectionString(string name, string defaultValue = null)
         {
             var possibleNames = Config.GetLocationizedNames(Location, name);
@@ -96,6 +111,21 @@ namespace Nvelope.Configuration
                 yield return name + "-dev";
             }
             yield return name;
+        }
+
+        /// <summary>
+        /// Get a list of the possible names that the setting could have in the config file, regardless
+        /// of the deployment location
+        /// </summary>
+        /// <param name="name"></param>
+        /// <returns></returns>
+        private static IEnumerable<string> GetLocationizedNames(string name)
+        {
+            var locs = Enum.GetNames(typeof(DeploymentLocation)).Select(l => l.ConvertTo<DeploymentLocation>());
+            var allVals = locs.SelectMany(l => GetLocationizedNames(l, name));
+            var res = allVals.Distinct();
+
+            return res;
         }
     }
 }
