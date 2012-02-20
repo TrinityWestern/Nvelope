ce0789236b8348036d5dce9fd6fcc325888a6b99
badjer <brian.dejong@gmail.com>
2012-02-20 11:51:22 -0800

    Added CommandLine for building command-line apps

---
+++ src/Nvelope/IO/CommandLine.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Nvelope;
+
+namespace Nvelope.IO
+{
+    /// <summary>
+    /// Represents a command entered at the command line or repl by a user
+    /// </summary>
+    public struct Command
+    {
+        public List<object> Args;
+        public Dictionary<string, object> Switches;
+        public HashSet<string> Flags;
+    }
+
+    /// <summary>
+    /// Provides methods for implementing a command-line or REPL from C#, and related utilities
+    /// </summary>
+    public static class CommandLine
+    {
+        /// <summary>
+        /// Takes a string of text (ie, from a command line or repl), and turns it into a Command struct.
+        /// Does automatic type conversion and checking, based on the expected format of the command
+        /// </summary>
+        /// <param name="commandText">The raw text from the command line</param>
+        /// <param name="argTypes">The types of the command arguments. If none supplied, all will be string. If there are more args than
+        /// supplied types, the args at the end with no supplied type will be string</param>
+        /// <param name="switches">A dict of the names to expected Types of the allowable switches. Switches are always -key value pairs,
+        /// and the switch name always must start with -</param>
+        /// <param name="flags">A set of the allowable flags. Flags have no type, they are either present or not</param>
+        /// <returns></returns>
+        public static Command Parse(string commandText, IEnumerable<Type> argTypes = null, Dictionary<string,Type> switches = null, IEnumerable<string> flags = null)
+        {
+            argTypes = argTypes ?? new Type[] { };
+            switches = switches ?? new Dictionary<string, Type>();
+            flags = flags ?? new string[]{};
+
+            var paramNames = switches.Keys.And(flags).ToSet();
+
+            var rawParts = commandText.Tokenize("^\\s*(\"[^\"]*\"|[^\\s]+)");
+            var parts = rawParts.Select(s => s.Trim('"')) // Anything that was quoted, unquote
+                // Anything that starts with - or -- and is a switch or flag, strip off the leading - or --
+                .Select(s => paramNames.Contains(s.TrimStart('-')) ? s.TrimStart('-') : s);
+
+            var suppliedFlags = parts.Intersect(flags);
+            var argsAndSwitches = parts.Except(flags).ToList();
+
+            // Which switches were actually supplied?
+            var suppliedSwitches = argsAndSwitches.Intersect(switches.Keys);
+            
+            // For each supplied switch, get it's location, as well as the location of its
+            // value (which is the next element in the list)
+            var switchLocs = suppliedSwitches.Select(s => argsAndSwitches.IndexOf(s));
+            var switchValLocs = switchLocs.Select(i => i + 1);
+
+            // Get a mapping of the switch names to the switch values, using the indexes
+            // we just calculated
+            var switchVals = switchLocs.Zip(switchValLocs,
+                (sl, vl) => new KeyValuePair<string,string>(argsAndSwitches[sl], argsAndSwitches[vl]))
+                .ToDictionary();
+
+            // Finally, convert all the switch values to the appropriate type
+            var typedSwitchVals = switchVals.Select(kv => new KeyValuePair<string, object>(kv.Key, kv.Value.ConvertTo(switches[kv.Key])))
+                .ToDictionary();
+
+            // The arguments are whatever is left after removing the switches and their values
+            var args = argsAndSwitches.ExceptIndicies(switchLocs.And(switchValLocs));
+            // Figure out what the types of the args should be
+            argTypes = argTypes.Take(args.Count(), typeof(string));
+            var typedArgs = args.Zip(argTypes, (val, type) => val.ConvertTo(type));
+
+            return new Command()
+            {
+                Args = typedArgs.ToList(),
+                Flags = suppliedFlags.ToSet(),
+                Switches = typedSwitchVals
+            };
+        }
+    }
+}
---
+++ test/Nvelope.Tests/IO/CommandLineTests.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+using Nvelope.IO;
+using Nvelope;
+using Nvelope.Reflection;
+
+namespace Nvelope.Tests.IO
+{
+    [TestFixture]
+    public class CommandLineTests
+    {
+        [Test]
+        public void Parse()
+        {
+            var res = CommandLine.Parse("a b c");
+            Assert.AreEqual("(a,b,c)", res.Args.Print());
+        }
+
+        [Test]
+        public void ParseQuotedArgs()
+        {
+            var res = CommandLine.Parse("a \"arg with space\" c");
+            Assert.AreEqual("(a,arg with space,c)", res.Args.Print());
+        }
+
+        [Test]
+        public void ParseTypedArgs()
+        {
+            var res = CommandLine.Parse("false 42 abc", typeof(bool).And(typeof(int)));
+            Assert.AreEqual("(False,42,abc)", res.Args.Print());
+            Assert.AreEqual(typeof(bool), res.Args.First().GetType());
+            Assert.AreEqual(typeof(int), res.Args.Second().GetType());
+            Assert.AreEqual(typeof(string), res.Args.Third().GetType());
+        }
+
+        [Test]
+        public void ParseFlags()
+        {
+            var res = CommandLine.Parse("-flag1 --flag2", flags: "flag1".And("flag2"));
+            Assert.AreEqual("(flag1,flag2)", res.Flags.Print());
+        }
+
+        [Test]
+        public void ParseSwitches()
+        {
+            var switches = new { s1 = "", s2 = false }._FieldTypes();
+            var res = CommandLine.Parse("-s1 val1 --s2 true", switches: switches);
+            Assert.AreEqual("([s1,val1],[s2,True])", res.Switches.Print());
+        }
+
+        [Test]
+        public void ParseTypedSwitches()
+        {
+            var switches = new { s1 = typeof(string), s2 = typeof(bool), s3 = typeof(int) }._AsDictionary().SelectVals(o => o as Type);
+            var res = CommandLine.Parse("-s1 a -s2 false --s3 42", switches: switches);
+            Assert.AreEqual("([s1,a],[s2,False],[s3,42])", res.Switches.Print());
+            Assert.AreEqual(res.Switches["s2"].GetType(), typeof(bool));
+            Assert.AreEqual(res.Switches["s3"].GetType(), typeof(int));
+        }
+
+        [Test]
+        public void ParseQuotedSwitches()
+        {
+            var switches = new { s1 = "", s2 = "" }._FieldTypes();
+            var res = CommandLine.Parse("-s1 abc -s2 \"switch with space\"", switches: switches);
+            Assert.AreEqual("([s1,abc],[s2,switch with space])", res.Switches.Print());
+        }
+
+        [Test]
+        public void ParseArgsAndFlags()
+        {
+            var res = CommandLine.Parse("a b --f1 c --f2", flags: "f1".And("f2"));
+            Assert.AreEqual("(a,b,c)", res.Args.Print());
+            Assert.AreEqual("(f1,f2)", res.Flags.Print());
+        }
+
+        [Test]
+        public void ParseArgsSwitchesFlags()
+        {
+            var switches = new { s1 = "", s2 = false }._FieldTypes();
+            var res = CommandLine.Parse("abc --f1 --s2 true 42 -s1 \"some text\" -f2",
+                typeof(string).And(typeof(int)),
+                switches,
+                "f1".And("f2"));
+
+            Assert.AreEqual("(abc,42)", res.Args.Print());
+            Assert.AreEqual("([s1,some text],[s2,True])", res.Switches.Print());
+            Assert.AreEqual("(f1,f2)", res.Flags.Print());
+        }
+
+    }
+}
