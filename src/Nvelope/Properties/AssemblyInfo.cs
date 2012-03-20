4eae9b06d0550672430277b8fc5bdbd9d3bb56b8
badjer <brian.dejong@gmail.com>
2012-03-20 12:21:04 -0700

    Updated nuget spec

--- src/Nvelope/Properties/AssemblyInfo.cs
+++ src/Nvelope/Properties/AssemblyInfo.cs
@@ -7,11 +7,11 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("Nvelope")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Nvelope is a set of language extensions for C# - a Batman utility belt to make Microsoft's favorite language faster, friendlier, and just a little bit sexier.")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Trinity Western University")]
 [assembly: AssemblyProduct("Nvelope")]
-[assembly: AssemblyCopyright("Copyright ©  2011")]
+[assembly: AssemblyCopyright("Copyright ©  2012")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -33,5 +33,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.0.0.1")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
