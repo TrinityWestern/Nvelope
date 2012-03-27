2017204ac6ad2b7764f1353fdf66d30e9cd1b7e0
badjer <brian.dejong@gmail.com>
2012-03-27 10:44:45 -0700

    Don't make it static

--- src/Nvelope/IO/CommandLine.cs
+++ src/Nvelope/IO/CommandLine.cs
@@ -4,21 +4,12 @@ using System.Linq;
 using System.Text;
 using Nvelope;
 using System.IO;
+using System.Reflection;
 
 namespace Nvelope.IO
 {
-    ///// <summary>
-    ///// Represents a command entered at the command line or repl by a user
-    ///// </summary>
-    //public struct Command
-    //{
-    //    public List<object> Args;
-    //    public Dictionary<string, object> Switches;
-    //    public HashSet<string> Flags;
-    //}
-
     public struct CommandArg
-    {
+    {  
         public string Name;
         public Type Type;
         public bool IsOptional;
@@ -55,7 +46,7 @@ namespace Nvelope.IO
     /// <summary>
     /// Provides methods for implementing a command-line or REPL from C#, and related utilities
     /// </summary>
-    public static class CommandLine
+    public class CommandLine
     {
         /// <summary>
         /// Convert a text command into a dict of arguments, based on the supplied expected arguments.
@@ -64,7 +55,7 @@ namespace Nvelope.IO
         /// <param name="commandText"></param>
         /// <param name="expectedArgs"></param>
         /// <returns></returns>
-        public static Dictionary<string, object> Parse(string commandText, IEnumerable<CommandArg> expectedArgs = null)
+        public Dictionary<string, object> Parse(string commandText, IEnumerable<CommandArg> expectedArgs = null)
         {
             expectedArgs = SanitizeArgs(expectedArgs);
 
@@ -92,7 +83,7 @@ namespace Nvelope.IO
             return converted.ToDictionary();
         }
 
-        public static IEnumerable<CommandArg> SanitizeArgs(IEnumerable<CommandArg> args)
+        public IEnumerable<CommandArg> SanitizeArgs(IEnumerable<CommandArg> args)
         {
             if (args == null)
                 return new CommandArg[] { };
@@ -102,13 +93,13 @@ namespace Nvelope.IO
                 .ToList();
         }
 
-        public static IEnumerable<string> Lex(string commandText)
+        public IEnumerable<string> Lex(string commandText)
         {
             return commandText.Tokenize("^\\s*(\"[^\"]*\"|[^\\s]+)")
                 .Select(s => s.Trim('"'));
         }
 
-        public static IEnumerable<ParseError> LexErrors(
+        public IEnumerable<ParseError> LexErrors(
             IEnumerable<string> tokens)
         {
             yield break;
@@ -120,7 +111,7 @@ namespace Nvelope.IO
         /// <param name="tokens"></param>
         /// <param name="flags">The names of fields that don't require a value</param>
         /// <returns></returns>
-        public static IEnumerable<KeyValuePair<string, string>> ParseArgs(IEnumerable<string> tokens, IEnumerable<string> flags)
+        public IEnumerable<KeyValuePair<string, string>> ParseArgs(IEnumerable<string> tokens, IEnumerable<string> flags)
         {
             if (!tokens.Any())
                 return null;
@@ -158,13 +149,13 @@ namespace Nvelope.IO
                 .And(ParseArgs(remainder, flags));
         }
 
-        public static IEnumerable<ParseError> ParseErrors(
+        public IEnumerable<ParseError> ParseErrors(
             IEnumerable<KeyValuePair<string, string>> parsedArgs)
         {
             yield break;
         }
 
-        public static IEnumerable<KeyValuePair<string,string>> AssignArgs(
+        public IEnumerable<KeyValuePair<string,string>> AssignArgs(
             IEnumerable<KeyValuePair<string,string>> parsedArgs, IEnumerable<CommandArg> expectedArgs)
         {
             var requiredArgs = expectedArgs.Where(a => !a.IsOptional);
@@ -181,7 +172,7 @@ namespace Nvelope.IO
                     yield return kv;
         }
 
-        public static IEnumerable<ParseError> AssignErrors(
+        public IEnumerable<ParseError> AssignErrors(
             IEnumerable<KeyValuePair<string, string>> parsedArgs, IEnumerable<CommandArg> expectedArgs)
         {
             if (!expectedArgs.Any())
@@ -205,7 +196,7 @@ namespace Nvelope.IO
             }
         }
 
-        public static IEnumerable<KeyValuePair<string, object>> ConvertArgs(
+        public IEnumerable<KeyValuePair<string, object>> ConvertArgs(
             IEnumerable<KeyValuePair<string, string>> parsedArgs, IEnumerable<CommandArg> expectedArgs)
         {
             foreach (var kv in parsedArgs)
@@ -226,7 +217,7 @@ namespace Nvelope.IO
             }
         }
 
-        public static IEnumerable<ParseError> ConvertErrors(
+        public IEnumerable<ParseError> ConvertErrors(
             IEnumerable<KeyValuePair<string, object>> convertedArgs, IEnumerable<CommandArg> expectedArgs)
         {
             if (!expectedArgs.Any())
@@ -245,78 +236,14 @@ namespace Nvelope.IO
             }
         }
 
-        public static bool IsName(string token)
+        public bool IsName(string token)
         {
             return token.StartsWith("-");
         }
 
-        public static string ToName(string token)
+        public string ToName(string token)
         {
             return token.TrimStart('-');
         }
-
-
-
-        ///// <summary>
-        ///// Takes a string of text (ie, from a command line or repl), and turns it into a Command struct.
-        ///// Does automatic type conversion and checking, based on the expected format of the command
-        ///// </summary>
-        ///// <param name="commandText">The raw text from the command line</param>
-        ///// <param name="argTypes">The types of the command arguments. If none supplied, all will be string. If there are more args than
-        ///// supplied types, the args at the end with no supplied type will be string</param>
-        ///// <param name="switches">A dict of the names to expected Types of the allowable switches. Switches are always -key value pairs,
-        ///// and the switch name always must start with -</param>
-        ///// <param name="flags">A set of the allowable flags. Flags have no type, they are either present or not</param>
-        ///// <returns></returns>
-        //public static Command Parse(string commandText, IEnumerable<Type> argTypes = null, Dictionary<string,Type> switches = null, IEnumerable<string> flags = null)
-        //{
-        //    argTypes = argTypes ?? new Type[] { };
-        //    switches = switches ?? new Dictionary<string, Type>();
-        //    flags = flags ?? new string[]{};
-
-        //    var paramNames = switches.Keys.And(flags).ToSet();
-
-        //    var rawParts = commandText.Tokenize("^\\s*(\"[^\"]*\"|[^\\s]+)");
-        //    var parts = rawParts.Select(s => s.Trim('"')) // Anything that was quoted, unquote
-        //        // Anything that starts with - or -- and is a switch or flag, strip off the leading - or --
-        //        .Select(s => paramNames.Contains(s.TrimStart('-')) ? s.TrimStart('-') : s);
-
-        //    var suppliedFlags = parts.Intersect(flags);
-        //    // Don't use Except below - Except returns the Set difference, which implicitly performs
-        //    // a distinct on the sequence.
-        //    var argsAndSwitches = parts.Where(p => !flags.Contains(p)).ToList();
-
-        //    // Which switches were actually supplied?
-        //    var suppliedSwitches = argsAndSwitches.Intersect(switches.Keys);
-            
-        //    // For each supplied switch, get it's location, as well as the location of its
-        //    // value (which is the next element in the list)
-        //    var switchLocs = suppliedSwitches.Select(s => argsAndSwitches.IndexOf(s));
-        //    var switchValLocs = switchLocs.Select(i => i + 1);
-
-        //    // Get a mapping of the switch names to the switch values, using the indexes
-        //    // we just calculated
-        //    var switchVals = switchLocs.Zip(switchValLocs,
-        //        (sl, vl) => new KeyValuePair<string,string>(argsAndSwitches[sl], argsAndSwitches[vl]))
-        //        .ToDictionary();
-
-        //    // Finally, convert all the switch values to the appropriate type
-        //    var typedSwitchVals = switchVals.Select(kv => new KeyValuePair<string, object>(kv.Key, kv.Value.ConvertTo(switches[kv.Key])))
-        //        .ToDictionary();
-
-        //    // The arguments are whatever is left after removing the switches and their values
-        //    var args = argsAndSwitches.ExceptIndicies(switchLocs.And(switchValLocs));
-        //    // Figure out what the types of the args should be - if we don't have enough, pad with string
-        //    argTypes = argTypes.Take(args.Count(), typeof(string));
-        //    var typedArgs = args.Zip(argTypes, (val, type) => val.ConvertTo(type));
-
-        //    return new Command()
-        //    {
-        //        Args = typedArgs.ToList(),
-        //        Flags = suppliedFlags.ToSet(),
-        //        Switches = typedSwitchVals
-        //    };
-        //}
-
     }
 }
