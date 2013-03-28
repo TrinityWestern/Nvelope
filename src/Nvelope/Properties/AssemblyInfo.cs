bca3e22d4fae4be655c2f538f454393939134195
badjer <brian.dejong@gmail.com>
2013-03-28 09:25:04 -0700

    Increment version #

--- src/Nvelope/Properties/AssemblyInfo.cs
+++ src/Nvelope/Properties/AssemblyInfo.cs
@@ -11,7 +11,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Trinity Western University")]
 [assembly: AssemblyProduct("Nvelope")]
-[assembly: AssemblyCopyright("Copyright ©  2012")]
+[assembly: AssemblyCopyright("Copyright ©  2013")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -33,5 +33,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.1.15")]
-[assembly: AssemblyFileVersion("1.0.1.15")]
+[assembly: AssemblyVersion("1.0.1.16")]
+[assembly: AssemblyFileVersion("1.0.1.16")]
