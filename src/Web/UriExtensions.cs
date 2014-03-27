d055487aabce155a0bed0df62ca9fbf6ef31b670
Bjørn Bulthuis <bjorn@bulthuis.org>
2014-03-27 15:02:15 -0700

    [UriExtensions] #21441 Added UriExtensions
    Added UriExtensions and tests:
    
    - AddParameter
    - ToStringWithoutSlash

---
+++ src/Web/UriExtensions.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web;
+
+namespace Nvelope.Web
+{
+    public static class UriExtensions
+    {
+        /// <summary>
+        /// Adds parameter to a uri.
+        /// </summary>
+        /// <param name="uri"></param>
+        /// <param name="key">Key of the parameter to add.</param>
+        /// <param name="value">Value of the parameter to add.</param>
+        /// <returns>Uri with added parameter.</returns>
+        public static Uri AddParameter(this Uri uri, string key, string value)
+        {
+            // Get current parameters
+            var uriBuilder = new UriBuilder(uri);
+            var query = HttpUtility.ParseQueryString(uriBuilder.Query);
+
+            // Add new parameter
+            query[key] = value;
+            uriBuilder.Query = query.ToString();
+
+            return uriBuilder.Uri;
+        }
+
+        /// <summary>
+        /// Output URI as a string without a slash at the end of the path
+        /// </summary>
+        /// <param name="uri"></param>
+        /// <returns>URI without ending slash</returns>
+        public static string ToStringWithoutSlash(this Uri uri)
+        {
+            var cleanUri = uri.ToStringN();
+
+            // Remove trailing slash
+            cleanUri = cleanUri.TrimEnd('/');
+
+            // Remove slash before query string
+            cleanUri = cleanUri.Replace("/?", "?");
+
+            // Remove slash before fragment
+            cleanUri = cleanUri.Replace("/#", "#");
+
+            return cleanUri;
+        }
+    }
+}
---
+++ test/Web/UriExtensionsTests.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+using Nvelope.Web;
+
+namespace Nvelope.Tests.Web
+{
+    [TestFixture]
+    class UriExtensionsTests
+    {
+        const string uriPlain = "http://test.com";
+        const string uriSubDomain = "http://sub.test.com";
+        const string uriSecure = "https://test.com";
+        const string uriScript = "http://test.com/script.php";
+        const string uriFriendly = "http://test.com/resource";
+        const string uriParam = "http://test.com?foo=1";
+        const string uriParams = "http://test.com?foo=1&bar=2";
+        const string uriNullParam = "http://test.com?foo";
+        const string uriFragment = "http://test.com#foo";
+
+        [TestCase(uriPlain,     Result = uriPlain)]
+        [TestCase(uriSubDomain, Result = uriSubDomain)]
+        [TestCase(uriSecure,    Result = uriSecure)]
+        [TestCase(uriScript,    Result = uriScript)]
+        [TestCase(uriFriendly,  Result = uriFriendly)]
+        [TestCase(uriParam,     Result = uriParam)]
+        [TestCase(uriParams,    Result = uriParams)]
+        [TestCase(uriNullParam, Result = uriNullParam)]
+        [TestCase(uriFragment,  Result = uriFragment)]
+        public string NoSlash(string uri)
+        {
+            return new Uri(uri).ToStringWithoutSlash();
+        }
+
+        [TestCase(uriPlain,     Result = uriPlain + "?test=3")]
+        [TestCase(uriSubDomain, Result = uriSubDomain + "?test=3")]
+        [TestCase(uriSecure,    Result = uriSecure + "?test=3")]
+        [TestCase(uriScript,    Result = uriScript + "?test=3")]
+        [TestCase(uriFriendly,  Result = uriFriendly + "?test=3")]
+        [TestCase(uriParam,     Result = uriParam + "&test=3")]
+        [TestCase(uriParams,    Result = uriParams + "&test=3")]
+        [TestCase(uriNullParam, Result = uriNullParam + "&test=3")]
+        [TestCase(uriFragment,  Result = "http://test.com?test=3#foo")]
+        public string AddParam(string uri)
+        {
+            return new Uri(uri).AddParameter("test", "3").ToStringWithoutSlash();
+        }
+    }
+}
